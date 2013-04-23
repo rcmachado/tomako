36f5fc790e27b9d617f71a3fe49160cb99d2e5d3
Rodrigo Machado <rcmachado@gmail.com>
2013-04-23 19:59:49 -0300

    Change version from dev to 0.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README_FILE = os.path.join(os.path.dirname(__file__), 'README.rst')
 
 setup(
     name='tomako',
-    version='dev',
+    version='0.1.0',
     description='Tomako is the easiest way to use Mako as a template engine '
                 'for Tornado',
     long_description=open(README_FILE).read(),
